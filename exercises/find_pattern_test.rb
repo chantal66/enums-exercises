cbf7f37aa874b5f5ddb3f7dd330ac7be16f447eb
chantaljustamond <cjustamm66@me.com>
2019-05-29 16:02:46 -0500

    find patteer test passing

--- exercises/find_pattern_test.rb
+++ exercises/find_pattern_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
@@ -5,7 +7,7 @@ require 'minitest/pride'
 class FindPatternTest < Minitest::Test
 
   def test_find_first_seven_letter_word
-    words = ["capricious", "berry", "unicorn", "bag", "apple", "festering", "pretzel", "pencil"]
+    words = %w[capricious berry unicorn bag apple festering pretzel pencil]
     found = nil
     words.each do |word|
       if word.length == 7
@@ -13,73 +15,114 @@ class FindPatternTest < Minitest::Test
         break
       end
     end
-    assert_equal "unicorn", found
+    assert_equal 'unicorn', found
   end
 
   def test_no_waldo
-    words = ["scarf", "sandcastle", "flag", "pretzel", "crow", "key"]
+    words = %w[scarf sandcastle flag pretzel crow key]
     found = nil
     words.each do |word|
-      # Your code goes here
+      if word == 'waldo'
+        found = 'waldo'
+        break
+      end
     end
     assert_equal nil, found
   end
 
   def test_find_waldo
-    skip
-    words = ["noise", "dog", "fair", "house", "waldo", "bucket", "fish"]
+    words = %w[noise dog fair house waldo bucket fish]
     found = nil
-    # Your code goes here
-    assert_equal "waldo", found
+    words.each do |word|
+      if word == 'waldo'
+        found = 'waldo'
+        break
+      end
+    end
+    assert_equal 'waldo', found
   end
 
   def test_cannot_find_3_letter_words
-    skip
-    words = ["piglet", "porridge", "bear", "blueberry"]
-    # Your code goes here
+    words = %w[piglet porridge bear blueberry]
+    found = nil
+    words.each do |word|
+      if word.length == 3
+        found = word
+        break
+      end
+    end
     assert_equal nil, found
   end
 
   def test_find_13
-    skip
     numbers = [2, 13, 19, 8, 3, 27]
-    # Your code goes here
+    found = nil
+    numbers.each do |num|
+      if num == 13
+        found = num
+        break
+      end
+    end
     assert_equal 13, found
   end
 
   def test_find_first_even_number
-    skip
     numbers = [3, 7, 13, 11, 10, 2, 17]
-    # Your code goes here
+    found = nil
+    numbers.each do |num|
+      if num.even?
+        found = num
+        break
+      end
+    end
     assert_equal 10, found
   end
 
   def test_find_first_multiple_of_3
-    skip
     numbers = [2, 8, 9, 27, 24, 5]
-    # Your code goes here
+    found = nil
+    numbers.each do |num|
+      if (num % 3).zero?
+        found = num
+        break
+      end
+    end
     assert_equal 9, found
   end
 
   def test_find_first_word_starting_with_q
-    skip
-    words = ["weirdo", "quill", "fast", "quaint", "quitter", "koala"]
-    # Your code goes here
-    assert_equal "quill", found
+    words = %w[weirdo quill fast quaint quitter koala]
+    found = nil
+    words.each do |word|
+      if word.start_with?('q')
+        found = word
+        break
+      end
+    end
+    assert_equal 'quill', found
   end
 
   def test_find_first_word_ending_with_er
-    skip
-    words = ["biggest", "pour", "blight", "finger", "pie", "border"]
-    # Your code goes here
-    assert_equal "finger", found
+    words = %w[biggest pour blight finger pie border]
+    found = nil
+    words.each do |word|
+      if word.end_with?('er')
+        found = word
+        break
+      end
+    end
+    assert_equal 'finger', found
   end
 
   def test_find_first_number_greater_than_20
-    skip
     numbers = [1, 8, 19, 21, 29, 31, 34]
-    # Your code goes here
+    found = nil
+    numbers.each do |num|
+      if num > 20
+        found = num
+        break
+      end
+    end
     assert_equal 21, found
   end
-
 end
