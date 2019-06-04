fd423d45f507443fcd55c49bf1ceca83b8bc3a3e
chantaljustamond <cjustamm66@me.com>
2019-06-04 10:20:42 -0500

    reduce and any tests passing

--- exercises/any_test.rb
+++ exercises/any_test.rb
@@ -6,52 +6,43 @@ class AnyTest < Minitest::Test
 
   def test_has_at_least_one_zero
     numbers = [2, 0, 9, 3, 0, 1]
-    has_zero = numbers.any? do |number|
-      number.zero?
-    end
+    has_zero = numbers.any?(&:zero?)
     assert has_zero
   end
 
   def test_does_not_have_any_zeros
     numbers = [3, 1, 3, 2, 4, 9, 8]
-    has_zero = numbers.any? do |number|
-      # Your code goes here
-    end
+    has_zero = numbers.any?(&:zero?)
     refute has_zero
   end
 
   def test_has_at_least_one_alice
-    skip
     names = ["Bill", "Bob", "Burton", "Alice", "Brandon"]
-    # Your code goes here
+    has_alice = names.any? { |name| name == 'Alice' }
     assert has_alice
   end
 
   def test_no_alices
-    skip
     names = ["Chuck", "Charlene", "Cory", "Chris", "Carl"]
-    # Your code goes here
+    has_alice = names.any? { |name| name == 'Alice' }
     refute has_alice
   end
 
   def test_has_a_multi_word_phrase
-    skip
     phrases = ["Sure!", "OK.", "I have no idea.", "Really?Whatever."]
-    # Your code goes here
+    has_multi_word_phrase = phrases.any? { |phrase| phrase.include? ' ' }
     assert has_multi_word_phrase
   end
 
   def test_no_monkeys
-    skip
     animals = ["elephant", "hippo", "jaguar", "python"]
-    # Your code goes here
+    has_monkeys = animals.any? { |animal| animal == 'monkeys' }
     refute has_monkeys
   end
 
   def test_no_multiples_of_five
-    skip
     numbers = [3, 1, 3, 2, 4, 9, 8]
-    # Your code goes here
+    multiples_of_5 = numbers.any? { |num| (num % 5).zero? }
     refute multiples_of_5
   end
 
