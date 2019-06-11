6db73e3b915f682cf3509bf78b6cb8b6d90cf993
chantaljustamond <cjustamm66@me.com>
2019-06-11 10:10:35 -0500

    none and one tests passing

--- exercises/none_pattern_test.rb
+++ exercises/none_pattern_test.rb
@@ -17,37 +17,37 @@ class NonePatternTest < Minitest::Test
     numbers = [9, 3, 3, 7, 6, -5, 1]
     not_none_negative = true
     numbers.each do |number|
-      # Your code goes here
+      # not_none_negative = false if number.to_s.include? '-'
+      not_none_negative = false unless number.negative?
     end
     refute not_none_negative
   end
 
   def test_none_are_negative
-    skip
     numbers = [9, 3, 1, 8, 3, 3, 5]
     none_negative = true
-    # Your code goes here
+    numbers.each { |num| none_negative = false if num.negative? }
     assert none_negative
   end
 
   def test_none_shall_pass
-    skip
     critters = ["elf", "hobbit", "dwarf", "wizard", "human"]
-    # Your code goes here
+    none_shall_pass = true
+    critters.each { |creature| none_shall_pass = false if creature == 'shall pass' }
     assert none_shall_pass
   end
 
   def test_one_or_more_shall_pass
-    skip
     phrases = ["go", "left", "can cross", "shall pass", "must stay", "tarried"]
-    # Your code goes here
+    none_shall_pass = true
+    phrases.each { |phrase| none_shall_pass = false unless phrase == 'shall pass' }
     refute none_shall_pass
   end
 
   def test_none_even
-    skip
     numbers = [3, 9, 15, 21, 19]
-    # Your code goes here
+    none_even = true
+    numbers.each { |num| none_even = false if num.even? }
     assert none_even
   end
 
